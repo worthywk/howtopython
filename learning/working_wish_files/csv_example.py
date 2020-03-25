1d1e0152075d21bbbbb3097c9d4bd1dace244794
ikriatov <worthywk@gmail.com>
2020-03-25 17:59:30 +0200

    work with files

--- learning/working_wish_files/csv_example.py
+++ learning/working_wish_files/csv_example.py
@@ -2,6 +2,7 @@ import csv
 from learning import hell
 
 File = "C:/users/ikriatov/file.csv"
+File1 = "C:/users/ikriatov/file1.csv"
 
 users = [
     ["Ihor", 28],
@@ -9,7 +10,15 @@ users = [
     ["Ann", 23]
 ]
 
+users1 = [
+    {'name': 'Ihor', 'age': 28},
+    {'name': 'Slavik', 'age': 29},
+    {'name': 'Ann', 'age': 23}
+]
 
+users1 = tuple(users1)
+
+# Operations for File
 with open(File, "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerows(users)
@@ -22,3 +31,26 @@ with open(File, "a", newline="") as file:
 with open(File, "r") as file:
     for item in file:
         hell.comma_separator(item, ',')
+
+with open(File, "r", newline="") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row[0], "-",row[1])
+
+# Operations for File1
+
+with open(File1, "w", newline="") as file1:
+    writer = csv.DictWriter(file1, fieldnames=('name', 'age'))
+    writer.writeheader()
+    writer.writerows(users1)
+
+    some_user = {'name': 'David','age': 15}
+    writer.writerow(some_user)
+
+with open(File1, "r", newline="") as file1:
+    reader = csv.DictReader(file1)
+    for user in reader:
+        print(user['name'], "-", user['age'])
+
+print(users)
+print(users1)
