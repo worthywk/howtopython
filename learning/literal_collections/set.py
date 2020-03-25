ceba3db3aa5ee145633f1c04b8cdd456770cef02
ikriatov <worthywk@gmail.com>
2020-03-25 13:24:12 +0200

    update set

--- learning/literal_collections/set.py
+++ learning/literal_collections/set.py
@@ -3,17 +3,26 @@ from learning import hell
 users = {"Ihor", "Slavik", "Ann", "David", "Ihor"}
 
 users1 = set(users)  # set with set
-users2 = set(("Ihor", "Slavik", "Ann"))  # set with list
-users3 = set(["Ihor", "Slavik", "Ann"])  # set with tuple
+users2 = set(("Ihor", "Slavik", "Ann"))  # set with tuple
+users3 = set(["Ihor", "Slavik", "Ann"])  # set with list
+
+hell.delete_item_in_list("Thor", users1)  # Thor is not in list
+users.discard("Ihor")
+print(users1)
+# {'Ihor', 'Ann', 'Slavik', 'David'}
+
 
 users2.add("David")
-print(users2)
 
-hell.delete_item_in_list("Thor", users1)
-users.discard("Ihor")
-print(sorted(users1))  # ['Ann', 'David', 'Ihor', 'Slavik']
+print("users2: {users2}, users3: {users3}".format(users2=users2, users3=users3))
+# users2: {'Ihor', 'Slavik', 'Ann'}, users3: {'Ihor', 'Slavik', 'Ann'}
 
-new_user = users3.union(users2)
-users3.update(users2)
-print(users3)
-print(new_user)
+
+
+print(users2.intersection((users3)))
+print(users2 & users3)
+# {'Ihor', 'Slavik', 'Ann'}
+
+print(users2.difference(users3))
+print(users2 - users3)
+# {'David'}
