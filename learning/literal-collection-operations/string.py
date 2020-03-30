b835cf191c9f38fcc92edb8890dbce8fae097f7d
ikriatov <worthywk@gmail.com>
2020-03-30 16:04:51 +0300

    add string exercises

--- learning/literal-collection-operations/string.py
+++ learning/literal-collection-operations/string.py
@@ -20,3 +20,17 @@ print ('.'.join(string.split()[0][:4].lower().capitalize()).replace('.'," "))
 # 'hell'.capitalize() - Hell
 # '.'.join(Hell) - H.e.l.l
 # 'H.e.l.l'.replace('.'," ") - H e l l
+
+
+### Exercises
+
+print('''
+Write a Python program to change a given string
+to a new string where the first and last chars
+have been exchanged''')
+
+string='Bomb'
+def change_string(string):
+	print(string[-1] + string[1:-1] + string[0])
+	
+change_string(string)  # bomB
